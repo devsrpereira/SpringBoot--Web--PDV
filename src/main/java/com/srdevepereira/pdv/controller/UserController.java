3d0967c7c4621ae927a3369d28f27a91488012dd
devsrpereira <srpereira@uni9.edu.br>
2023-07-18 20:51:49 -0300

    CRUD UserController criado

---
+++ src/main/java/com/srdevepereira/pdv/controller/UserController.java
@@ -0,0 +1,61 @@
+package com.srdevepereira.pdv.controller;
+
+import com.srdevepereira.pdv.entity.User;
+import com.srdevepereira.pdv.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private UserRepository userRepository;
+
+    public UserController(@Autowired UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping() // retorna todos os usuarios cadastrados
+    public ResponseEntity getAll(){
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping() // criar novo usuario
+    public ResponseEntity post(@RequestBody User user){
+        try{
+            user.setEnabled(true);
+            return new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
+        }
+        catch (Exception error){
+            return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping() //atualiza usuarios já cadastrados
+    public ResponseEntity put(@RequestBody User user){
+        Optional<User> userToEdit = userRepository.findById(user.getId());
+
+        if(userToEdit.isPresent()){
+            userRepository.save(user);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+
+        return ResponseEntity.notFound().build(); //usamos quando queremos definir um padrão de tratativa de erro vazia
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity delete(@PathVariable Long id){
+        try{
+            userRepository.deleteById(id);
+            return new ResponseEntity<>("Usuario removido com sucesso", HttpStatus.OK);
+        }
+        catch (Exception error){
+            return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
