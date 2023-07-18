ab5df8006d21f8be5872eccb9d799aced45147d0
devsrpereira <srpereira@uni9.edu.br>
2023-07-18 16:49:40 -0300

    Projeto PDV Web iniciado

---
+++ src/main/java/com/srdevepereira/springbootwebpdv/SpringBootWebPdvApplication.java
@@ -0,0 +1,13 @@
+package com.srdevepereira.springbootwebpdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebPdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebPdvApplication.class, args);
+	}
+
+}
